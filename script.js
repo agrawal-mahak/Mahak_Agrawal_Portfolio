17cd82430901d374815791c0d234bcdf4f76c682
agrawal-mahak <agrawalmahak7@gmail.com>
2026-09-23 15:42:46 +0530

    top navigation arrow added

--- script.js
+++ script.js
@@ -816,3 +816,23 @@ function askAiQuestion(key) {
   }, 700);
 }
 window.askAiQuestion = askAiQuestion;
+
+/* Floating Scroll-To-Top Navigation Logic */
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+if (scrollToTopBtn) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.classList.add("show");
+    } else {
+      scrollToTopBtn.classList.remove("show");
+    }
+  });
+
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
